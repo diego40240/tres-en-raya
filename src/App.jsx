7e27cb2a5a5a7d3c8153f39d743b8874f3db95ae
diego <diego40240@hotmail.com>
2023-05-19 18:23:12 -0500

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+const TURNS = {
+  x: "x",
+  O: "o",
+};
+
+// TABLERO DE 3 EN RAYA
+const board = Array(9).fill(null);
+
+function App() {
+  return (
+    <main className="board">
+      <h1>hola DIEGO</h1>
+      <section className="game">
+
+      </section>
+    </main>
+  );
+}
+
+export default App;
