03b26293d434da09af90bcbfe1ef89e3c164f99c
diego <diego40240@hotmail.com>
2023-05-19 19:15:49 -0500

    agregado de funcion square

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,21 @@ const TURNS = {
 // TABLERO DE 3 EN RAYA
 const board = Array(9).fill(null);
 
+const Square = ({ children, updateBoard, index }) => {
+  return <div className="square">{children}</div>;
+};
 function App() {
   return (
     <main className="board">
       <h1>hola DIEGO</h1>
       <section className="game">
-
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
+              {index}
+            </Square>
+          );
+        })}
       </section>
     </main>
   );
